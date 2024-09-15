eed853901009405f75233b1d3bd523f10ec84664
amardip padghan <82278378+amardippadghan@users.noreply.github.com>
2024-09-15 22:59:17 +0530

    update package.json and App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from "./components/Header";
-import AnimatedCursor from "react-animated-cursor";
 import { useSelector } from "react-redux";
 import Footer from "./components/Footer";
 import Overview from './components/Overview/Overview';
