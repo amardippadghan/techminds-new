c3a8738e23e6d26d4669de74a73fd410c99e9363
amardip padghan <82278378+amardippadghan@users.noreply.github.com>
2024-09-15 18:34:23 +0530

    update App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import { useSelector } from "react-redux";
 
 
 function App() {
-    const theme = useSelector((state) => state.theme.theme); // Get the current theme from Redux state
+    const theme = useSelector((state) => state.theme.theme); 
 
-    // Update HTML `data-theme` attribute based on the Redux state
+   
     useEffect(() => {
       document.documentElement.setAttribute("data-theme", theme);
     }, [theme]);
@@ -15,12 +15,12 @@ function App() {
     <>
       <AnimatedCursor
         innerSize={8}
-        outerSize={14} // Slightly bigger outer size for smoother outer transition
+        outerSize={14}
         color="193, 11, 111"
-        outerAlpha={0.3} // Slightly increase the transparency
-        innerScale={0.8} // More responsive inner scaling for smooth movement
-        outerScale={6} // A bit more noticeable outer scaling for hover effects
-        hasBlendMode={true} // Enable blend mode for a smoother look
+        outerAlpha={0.3}
+        innerScale={0.8} 
+        outerScale={6}
+        hasBlendMode={true} 
         clickables={[
           "a",
           'input[type="text"]',
@@ -37,16 +37,15 @@ function App() {
             target: ".custom",
             options: {
               innerSize: 16,
-              outerSize: 16, // Larger outer size for custom targets
-              color: "255, 255, 255", // White color for custom hover targets
-              outerAlpha: 0.4, // More noticeable alpha for custom targets
-              innerScale: 1, // Slightly larger inner scale
-              outerScale: 5, // More pronounced outer scale for custom targets
+              outerSize: 16,
+              color: "255, 255, 255", 
+              outerAlpha: 0.4, 
+              innerScale: 1, 
+              outerScale: 5, 
             },
           },
         ]}
       />
-
       <Header />
     </>
   );
